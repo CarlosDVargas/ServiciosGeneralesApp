ff39ac739427311070d0708c760f28167a8f6ae8
Carlos Daniel Vargas Varela <carlos.vargas@ucrso.info>
2022-03-05 18:22:17 -0600

    Se agregaron las validaciones para los elementos de la tabla Request

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,15 @@
 class Request < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  before_save { self.requester_mail = requester_mail.downcase }
+  validates :requester_name, presence: true
+  validates :requester_extension, presence: true
+  validates :requester_phone, presence: true
+  validates :requester_id, presence: true
+  validates :requester_mail, presence: true,
+            format: { with: VALID_EMAIL_REGEX }
+  validates :requester_type, presence: true
+  validates :work_building, presence: true
+  validates :work_location, presence: true
+  validates :work_type, presence: true
+  validates :work_description, presence: true
 end
