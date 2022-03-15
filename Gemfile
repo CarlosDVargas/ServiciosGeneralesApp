02492fdc6a3cfe60736abb9ac00c7db004914b96
Carlos Daniel Vargas Varela <carlos.vargas@ucrso.info>
2022-03-14 18:10:35 -0600

    Se reinstala bootstrap usando el gem respectivo, así como se arregla problemas de carga desde el cliente.

--- Gemfile
+++ Gemfile
@@ -51,8 +51,11 @@ gem 'tzinfo-data'
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
 
+# gem "bootstrap"
+gem 'bootstrap', '~> 5.1', '>= 5.1.3'
+
 # Use Sass to process CSS
-# gem "sassc-rails"
+gem "sassc-rails"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,5 +7,5 @@ import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
-// import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
-// lazyLoadControllersFrom("controllers", application)
+//import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
+//lazyLoadControllersFrom("controllers", application)
