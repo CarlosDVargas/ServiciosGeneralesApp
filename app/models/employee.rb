c6f6098720d64e262aa6a21e44d5715e8958988f
Carlos Daniel Vargas Varela <carlos.vargas@ucrso.info>
2022-03-19 08:34:52 -0600

    Se arregla error con relación Employee-User

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,7 +2,7 @@ class Employee < ApplicationRecord
   has_many :tasks
   has_many :requests, through: :task
 
-  belongs_to :user, optional: true
+  has_one :user
 
   validates :email, :uniqueness => {message: "ya pertenece a otra cuenta"}
 end
